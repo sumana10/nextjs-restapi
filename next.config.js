087364451827004d1a435315c88f145ae2e9313b
sumana10 <sumanaonlyme@gmail.com>
2022-09-05 02:46:36 +0530

    git init

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env:{
+    MONGO_URI:"mongodb+srv://sumana:sumana1005@cluster0.jp2iap8.mongodb.net/nextdb?retryWrites=true&w=majority"
+  }
 }
 
 module.exports = nextConfig
